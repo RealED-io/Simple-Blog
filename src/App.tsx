3c0b3578b3d792aa5717b20276025c39bb1489be
RealED-io <ed.realuyo@gmail.com>
2025-05-22 07:22:04 +0800

    Added session getter

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,37 @@ import './App.css';
 import { LoginForm } from './components/LoginForm.tsx';
 import { SignupForm } from './components/SignupForm.tsx';
 import { LogoutButton } from './components/LogoutButton.tsx';
+import { supabase } from './supabaseClient.ts';
+import { useEffect, useState } from 'react';
+import type { Session } from '@supabase/supabase-js';
 
 function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) {
+        alert(error.message);
+      } else {
+        setSession(data.session);
+      }
+    };
+    fetchSession();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      },
+    );
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+
+  }, []);
+
+  console.log(session);
 
   return (
     <div>
